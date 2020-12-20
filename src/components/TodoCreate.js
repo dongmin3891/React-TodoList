9ef65cbb9767d67f9d27ebf8a7e947c2eb11c5c0
dongmin <rlaehdals753@naver.com>
2020-12-20 13:54:25 +0900

    TodoList - 프로젝트 생성 UI 준비 - 2 TodoCreate.js - InsertForm

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { MdAdd } from "react-icons/md";
 
+/*  BUTTON */
 const CircleButton = styled.button`
   background: #38d9a9;
   &:hover {
@@ -30,13 +31,54 @@ const CircleButton = styled.button`
 
   border: none;
   outline: none;
+
+  transition: 0.125s all ease-in;
+  ${(props) =>
+    props.open &&
+    css`
+      background: #ff6b6b;
+      &:hover {
+        background: #ff8787;
+      }
+      &:active {
+        background: #fa5252;
+      }
+      transform: translate(-50%, 50%) rotate(45deg);
+    `}
+`;
+
+/* 특점 FORM 위치를 정해줌 */
+const InsertFormPositioner = styled.div`
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+`;
+
+const InsertForm = styled.div`
+  background: #f8f9fa;
+  padding: 32px;
+  padding-bottom: 72px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-top: 1px solid #e9ecef;
 `;
 
 function TodoCreate() {
+  const [open, setOpen] = useState(false);
+  const onToggle = () => setOpen(!open);
+
   return (
-    <CircleButton>
-      <MdAdd />
-    </CircleButton>
+    <>
+      {open && (
+        <InsertFormPositioner>
+          <InsertForm>...</InsertForm>
+        </InsertFormPositioner>
+      )}
+      <CircleButton onClick={onToggle} open={open}>
+        <MdAdd />
+      </CircleButton>
+    </>
   );
 }
 
